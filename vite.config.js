6467def19ca3b93fe2e9341bcfbee63066288e07
lancelotprd <lancelot.prd@gmail.com>
2026-03-12 04:17:36 +0100

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // IMPORTANT : Remplacez 'NOM_DE_VOTRE_DEPOT' par le nom de votre projet sur GitHub
-  base: '/portfolio-lancelot-cavalec/', 
-})
